a14f9df13eef34664ed036e06149bbb8d983728d
tapan <tapan@ubuntu.ubuntu-domain>
2013-05-14 14:50:45 +0530

    sol. to pe17

--- pe17.py
+++ pe17.py
@@ -1,33 +1,41 @@
-a=[]
-a.append(0)
-a.append(3)
-a.append(3)
-a.append(5)
-a.append(4)
-a.append(4)
-a.append(3)
-a.append(5)
-a.append(5)
-a.append(4)
-a.append(3)          #ten till here
-a.append(6)
-a.append(6)         
-a.append(8)
-a.append(8)
-a.append(7)
-a.append(7)
-a.append(9)
-a.append(9)
-a.append(8)
-a.append(6)         #twenty till here
+#leaving 1000 for the time being
+one = 191 #unit digit + hundred d of 3d + 1000
+two = 190 
+three = 190
+four = 190
+five = 190
+six = 190
+seven = 190
+eight = 190
+nine = 190
+ten = 10
+eleven = 10
+twelve = 10
+thirteen = 10
+fourteen = 10
+fifteen = 10
+sixteen = 10
+seventeen = 10
+eighteen = 10
+nineteen = 10 
+twenty = 100 
+thirty = 100
+forty = 100
+fifty = 100
+sixty = 100
+seventy = 100
+eighty = 100
+ninety = 100
+hundred = 900
+nd = 891
+thousand = 1
 
-''' after this not done
+total = one*3 + two*3 + three*5 + four*4 + five*4 + six*3 + seven*5 + eight*5 + nine*4 
+total = total + ten*3 + eleven*6 + twelve*6 + thirteen*8 + fourteen*8 + fifteen*7 + sixteen*7 + seventeen*9 + eighteen*8 + nineteen*8 
+total = total + twenty*6 + thirty*6 + forty*5 + fifty*5 + sixty*5 + seventy*7 + eighty*6 + ninety*6 + hundred*7 + nd*3 + thousand*8
 
-a.append(4)
-a.append(4)
-a.append(4)
-a.append(4)
-a.append(4)
-a.append(4)
-a.append(4)
-'''
+print total    
+
+
+
+
