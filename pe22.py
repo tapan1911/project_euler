d0654da806d6db4e8950798bad607767b8e033dd
tapan <tapan@ubuntu.ubuntu-domain>
2013-05-17 23:35:33 +0530

    project euler solutions

--- pe22.py
+++ pe22.py
@@ -1,15 +1,44 @@
-import sys
-a=[][]
-f=open('names.txt','r')
+import time
+start=time.time()
+f=open("names.txt",'r')
+ 
+for x in f:
+	data=x
+
+j=0
+y=len(data)
+flag=0
+b=[]
+txt=''
+while 1:
+	if(data[j]=="\""):
+		if(flag==0):
+			flag+=1
+		else:
+			b.append(txt)
+			flag=0
+			txt=''
+	
+	if(ord(x[j])-ord('A')>=0 and ord(x[j])-ord('A')<=26):
+		txt=txt+x[j]	
+
+	j+=1
+	if(j==y):
+		break
+
+
+b.sort()
+y=len(b)
 total=0
+for i in range(0,y):
+	x=b[i]
+	sum=0
+	for z in x:
+		sum=sum+ord(z)-ord('A')+1
+	sum=sum*(i+1)
+	total=total+sum
 
-while(1):
-	c=f.read(1)
-	if(c=="\""):
-		ch=f.read(1)
-		while(ch!="\""):
-			a.append(ch)
+elapsed = time.time() - start
+print "Ans = ",total
+print "Time taken to find the result = %s seconds"%elapsed
 
-
-	if(c==EOF):
-		sys.exit(0)	
