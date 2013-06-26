7770b62acafcbe5d0ceea3a7526bfa0c71b6a031
tapan <tapan1911@gmail.com>
2013-06-26 19:05:03 +0530

    Update pe31.py

--- pe31.py
+++ pe31.py
@@ -1,29 +1,12 @@
 import time
-start=time.time()
-count=1
-for i in range(0,201,100):
-	for j in range(0,201,50):
-		if(i+j>200):
-			break
-		for k in range(0,201,20):
-			if(i+j+k>200):
-        	                break
-	
-			for l in range(0,201,10):
-				if(i+j+k+l>200):
-        		                break
-	
-				for m in range(0,201,5):
-					if(i+j+k+l+m>200):
-        			                break
-	
-					for n in range(0,201,2):
-						if(i+j++k+l+m+n>200):
-        				                break
-	
-						if(i+j+k+l+m+n<=200):
-							count+=1
 
-elapsed=time.time()-start
-print count
-print "time taken to find the result is %s seconds"%elapsed
+start = time.time()
+a = [1,2,5,10,20,50,100,200]
+b = [1] + [0]*200
+for n in a:
+        for i in range(n,201):
+                b[i]+= b[i-n]
+
+elapsed = time.time() - start
+print "Ans = ",b[200]
+print "Time taken to find the result = %s seconds"%elapsed
